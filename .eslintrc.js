46a651d10b1c84dac7e16dd966c45c48b98f2fda
MiG <yanrancanfei@163.com>
2023-07-11 16:08:40 +0800

    feat(eslint): 配置规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,34 @@
 module.exports = {
-    env: {
-        node: true,
-        commonjs: true,
-        es2021: true,
-        jest: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-        ecmaVersion: 12,
-    },
-    rules: {
-        'no-undef': 'error',
-        'prefer-const': [
-            'error',
-            {
-                destructuring: 'any',
-                ignoreReadBeforeAssign: false,
-            },
-        ],
-        quotes: ['error', 'single'],
-        'no-console': 'error',
-        'no-unused-vars': 'error',
-        semi: ['error', 'always'],
-        'no-var': 'error',
-        'eol-last': ['error', 'always'],
-        'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
-        'lines-around-comment': ['error', { beforeLineComment: false, allowBlockStart: true }],
-    },
+  root: true,
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'no-undef': 'error',
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: false,
+      },
+    ],
+    quotes: ['error', 'single'],
+    'no-console': 'error',
+    'no-unused-vars': 'error',
+    semi: ['error', 'always'],
+    'no-var': 'error',
+    'eol-last': ['error', 'always'],
+    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
+    'lines-around-comment': [
+      'error',
+      { beforeLineComment: false, allowBlockStart: true },
+    ],
+  },
 };
